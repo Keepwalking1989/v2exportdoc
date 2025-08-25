27387cabbe4e86ced932a90628e3e6dcf818742a
john mcclain <john.mcclainpramotion@gmail.com>
2025-08-25 10:24:30 +0000

    conversationRate.toFixed is not a function this error occur

--- src/lib/custom-invoice-pdf.ts
+++ src/lib/custom-invoice-pdf.ts
@@ -234,7 +234,7 @@ function drawCustomInvoice(
     const discountUSD = docData.discount || 0;
     const finalTotalUSD = grandTotalAmountUSD + freightUSD - discountUSD;
     
-    const conversationRate = docData.conversationRate || 0;
+    const conversationRate = parseFloat(String(docData.conversationRate)) || 0;
     const totalAmountInr = finalTotalUSD * conversationRate;
     
     const gstString = docData.gst || "0";
